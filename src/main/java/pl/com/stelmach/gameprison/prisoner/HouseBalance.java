0359a259493f89389b9a1119fb61cdf916be31b7
mateusz <matistelmach@gmail.com>
2019-07-01 10:40:44 +0200

    commit 0.0.13 refactor houseBalance fun

--- src/main/java/pl/com/stelmach/gameprison/prisoner/HouseBalance.java
+++ src/main/java/pl/com/stelmach/gameprison/prisoner/HouseBalance.java
@@ -24,36 +24,17 @@ public class HouseBalance {
 
     private void houseBalanceCalc(BirthChart birthChart) {
         for (Houses houses : Houses.values()) {
-            int houseNumber = houses.getPositionNumber();
             int strength = houseBalance.getOrDefault(houses, 0);
-            int housePositionStart = birthChart.getHousePositionInt(houseNumber);
-            int housePostionEnd;
-            if (houses.getPositionNumber() == 11) {
-                housePostionEnd = birthChart.getHousePositionInt(0);
-            } else housePostionEnd = birthChart.getHousePositionInt(houseNumber + 1);
-
-
-            if (housePostionEnd > housePositionStart) {
-                for (HeavenCelestial heavenCelestial : HeavenCelestial.values()) {
-                    int planetPosition = birthChart.getPlanetPositionInt(heavenCelestial.getName());
-                    if (Range.between(housePositionStart, housePostionEnd)
-                            .contains(planetPosition)) {
-                        strength += heavenCelestial.getValueToAdd();
-                    }
+            for (HeavenCelestial heavenCelestial : HeavenCelestial.values()) {
+                int planetPosition = birthChart.getPlanetPositionInt(heavenCelestial.getName());
+                if (houses.isInHouse(birthChart, planetPosition)) {
+                    strength += heavenCelestial.getValueToAdd();
                 }
-                houseBalance.put(houses, strength);
-            } else {
-
-                for (HeavenCelestial heavenCelestial : HeavenCelestial.values()) {
-                    int planetPosition = birthChart.getPlanetPositionInt(heavenCelestial.getName());
-                    if (Range.between(housePositionStart, 360)
-                            .contains(planetPosition) || Range.between(0, housePostionEnd).contains(planetPosition)) {
-                        strength += heavenCelestial.getValueToAdd();
-                    }
-                }
-                houseBalance.put(houses, strength);
-
             }
+            houseBalance.put(houses, strength);
         }
+
     }
 }
+
+
