5a8eb5ab7213e02de76829724f4938cd631e38fa
mateusz <matistelmach@gmail.com>
2019-07-01 10:26:06 +0200

    commit 0.0.11 isInHouse fun add to enum Houses

--- src/main/java/pl/com/stelmach/gameprison/astrologyinfluance/Houses.java
+++ src/main/java/pl/com/stelmach/gameprison/astrologyinfluance/Houses.java
@@ -2,27 +2,152 @@ package pl.com.stelmach.gameprison.astrologyinfluance;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.apache.commons.lang3.Range;
 import pl.com.stelmach.gameprison.prisoner.CharacterQuality;
 
 @AllArgsConstructor
 @Getter
 public enum Houses {
-    FIRST(0, 3, CharacterQuality.SELF_CENTERED), //positionNumber  of house is house number h -1, becouse it is the way AstrologyApi lists
-    SECOND(1, 2, CharacterQuality.WEALTH_CAPACITY),// houses in function getCupsPosition
-    THIRD(2, 1, CharacterQuality.COMMUNICATION_SKILLS),
-    FOURTH(3, 3, CharacterQuality.FAMILY_BOUND_STRENGTH),
-    FIFTH(4, 2, CharacterQuality.CHILDREN_POSSESSION),
-    SIXTH(5, 1, CharacterQuality.WORK_ABILITY),
-    SEVENTH(6, 3, CharacterQuality.MARRIAGE_POTENTIAL),
-    EIGTH(7, 2, CharacterQuality.DRIVE_TO_CRIME),
-    NINTH(8, 1, CharacterQuality.EDUCTAION_POTENTIAL),
-    TENTH(9, 3, CharacterQuality.SOCIAL_STATUS),
-    ELEVENTH(10, 2, CharacterQuality.FRIENDS_IN_HIGH_PLACES),
-    TWELFTH(11, 1, CharacterQuality.ENEMIES_ATTACKS);
+    FIRST(0, 3, CharacterQuality.SELF_CENTERED) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(0);
+            int houseEnd = birthChart.getHousePositionInt(1) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    SECOND(1, 2, CharacterQuality.WEALTH_CAPACITY) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(1);
+            int houseEnd = birthChart.getHousePositionInt(2) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    THIRD(2, 1, CharacterQuality.COMMUNICATION_SKILLS) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(2);
+            int houseEnd = birthChart.getHousePositionInt(3) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    FOURTH(3, 3, CharacterQuality.FAMILY_BOUND_STRENGTH) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(3);
+            int houseEnd = birthChart.getHousePositionInt(4) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    FIFTH(4, 2, CharacterQuality.CHILDREN_POSSESSION) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(4);
+            int houseEnd = birthChart.getHousePositionInt(5) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    SIXTH(5, 1, CharacterQuality.WORK_ABILITY) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(5);
+            int houseEnd = birthChart.getHousePositionInt(6) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    SEVENTH(6, 3, CharacterQuality.MARRIAGE_POTENTIAL) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(6);
+            int houseEnd = birthChart.getHousePositionInt(7) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    EIGTH(7, 2, CharacterQuality.DRIVE_TO_CRIME) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(7);
+            int houseEnd = birthChart.getHousePositionInt(8) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    NINTH(8, 1, CharacterQuality.EDUCTAION_POTENTIAL) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(8);
+            int houseEnd = birthChart.getHousePositionInt(9) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    TENTH(9, 3, CharacterQuality.SOCIAL_STATUS) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(9);
+            int houseEnd = birthChart.getHousePositionInt(10) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    ELEVENTH(10, 2, CharacterQuality.FRIENDS_IN_HIGH_PLACES) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(10);
+            int houseEnd = birthChart.getHousePositionInt(11) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    },
+    TWELFTH(11, 1, CharacterQuality.ENEMIES_ATTACKS) {
+        @Override
+        public boolean isInHouse(BirthChart birthChart, int position) {
+            int houseStart = birthChart.getHousePositionInt(11);
+            int houseEnd = birthChart.getHousePositionInt(0) - 1;
+            if (houseStart < houseEnd) {
+                return startHousesmallerThanEndHouse(houseStart, houseEnd, position);
+            } else return startHousebiggerThanEndhouse(houseStart, houseEnd, position);
+        }
+    };
+
+    private static boolean startHousebiggerThanEndhouse(int houseStart, int houseEnd, int position) {
+        if (Range.between(houseStart, 360).contains(position) ||
+                Range.between(0, houseEnd).contains(position)) return true;
+        return false;
+    }
+
+    private static boolean startHousesmallerThanEndHouse(int houseStart, int houseEnd, int position) {
+        if (Range.between(houseStart, houseEnd).contains(position)) return true;
+        else return false;
+    }
 
 
     private final int positionNumber;
     private final int valueToAdd;
     private final CharacterQuality quality;
 
+    public abstract boolean isInHouse(BirthChart birthChart, int position);
+
+
 }
+
+
