c0f805fbfc9884ca8c4ff5c3e8ec71c38697b029
mateusz <matistelmach@gmail.com>
2019-07-12 13:22:18 +0200

    refactor FourElement and ElementBalance

--- src/main/java/pl/com/stelmach/gameprison/astrologyinfluance/FourElements.java
+++ src/main/java/pl/com/stelmach/gameprison/astrologyinfluance/FourElements.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import pl.com.stelmach.gameprison.person.CharacterQuality;
 
-import java.util.Arrays;
-
 
 @AllArgsConstructor
 @Getter
@@ -15,6 +13,14 @@ public enum FourElements {
         public boolean containsElement(int position) {
             return ZodiacSign.ARIES.isInSign(position) || ZodiacSign.lEO.isInSign(position) || ZodiacSign.SIGGITARIUS.isInSign(position);
         }
+
+        @Override
+        public int elementStrength(BirthChart birthChart) {
+            int strength = 0;
+            strength = elementStrengthbyPlanet(FIRE, birthChart, strength);
+            strength += FourElements.elementStrengthbyHouses(FIRE, birthChart, strength);
+            return strength;
+        }
     },
     AIR("air", CharacterQuality.INTELLECTUAL_CAPACITY) {
         @Override
@@ -22,17 +28,41 @@ public enum FourElements {
             return ZodiacSign.LIBRA.isInSign(position) || ZodiacSign.GEMINI.isInSign(position) || ZodiacSign.AQUARIUS.isInSign(position);
 
         }
+
+        @Override
+        public int elementStrength(BirthChart birthChart) {
+            int strength = 0;
+            strength = elementStrengthbyPlanet(AIR, birthChart, strength);
+            strength += FourElements.elementStrengthbyHouses(AIR, birthChart, strength);
+            return strength;
+        }
     },
     WATER("water", CharacterQuality.EMOTIONAL_RESPOSIVENESS) {
         @Override
         public boolean containsElement(int position) {
             return ZodiacSign.CANCER.isInSign(position) || ZodiacSign.PISCES.isInSign(position) || ZodiacSign.SCORPIO.isInSign(position);
         }
+
+        @Override
+        public int elementStrength(BirthChart birthChart) {
+            int strength = 0;
+            strength = elementStrengthbyPlanet(WATER, birthChart, strength);
+            strength += FourElements.elementStrengthbyHouses(WATER, birthChart, strength);
+            return strength;
+        }
     },
     EARTH("earth", CharacterQuality.MATERIAL_POSSESSIVENESS) {
         @Override
         public boolean containsElement(int position) {
             return ZodiacSign.CAPRICORN.isInSign(position) || ZodiacSign.VIRGO.isInSign(position) || ZodiacSign.TAURUS.isInSign(position);
+        }
+
+        @Override
+        public int elementStrength(BirthChart birthChart) {
+            int strength = 0;
+            strength = elementStrengthbyPlanet(EARTH, birthChart, strength);
+            strength += FourElements.elementStrengthbyHouses(EARTH, birthChart, strength);
+            return strength;
         }
     };
 
@@ -42,6 +72,8 @@ public enum FourElements {
 
     public abstract boolean containsElement(int position);
 
+    public abstract int elementStrength(BirthChart birthChart);
+
     public static FourElements whatElement(int position) {
         if (FourElements.FIRE.containsElement(position)) return FourElements.FIRE;
         if (FourElements.AIR.containsElement(position)) return FourElements.AIR;
@@ -49,4 +81,21 @@ public enum FourElements {
         if (FourElements.EARTH.containsElement(position)) return FourElements.EARTH;
         throw new IllegalArgumentException("element not found for position, position needs to be 0-359 int");
     }
+
+    private static int elementStrengthbyPlanet(FourElements element, BirthChart birthChart, int strength) {
+        for (HeavenCelestial planet : HeavenCelestial.values()) {
+            int position = birthChart.getPlanetPositionInt(planet.getName());
+            if (element.containsElement(position)) strength += planet.getValueToAdd();
+        }
+        return strength;
+    }
+
+    private static int elementStrengthbyHouses(FourElements element, BirthChart birthChart, int strength) {
+        for (Houses houses : Houses.values()) {
+            int position = birthChart.getHousePositionInt(houses.getPositionNumber());
+            if (element.containsElement(position)) strength += houses.getValueToAdd();
+        }
+        return strength;
+    }
+
 }
--- src/main/java/pl/com/stelmach/gameprison/person/ElementBalance.java
+++ src/main/java/pl/com/stelmach/gameprison/person/ElementBalance.java
@@ -4,8 +4,6 @@ package pl.com.stelmach.gameprison.person;
 import lombok.Getter;
 import pl.com.stelmach.gameprison.astrologyinfluance.BirthChart;
 import pl.com.stelmach.gameprison.astrologyinfluance.FourElements;
-import pl.com.stelmach.gameprison.astrologyinfluance.HeavenCelestial;
-import pl.com.stelmach.gameprison.astrologyinfluance.Houses;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,25 +15,9 @@ public class ElementBalance {
 
     public ElementBalance(BirthChart birthChart) {
         elementBalance = new HashMap<>();
-        elementBalanceCalc(birthChart);
-
-    }
-
-    private void elementBalanceCalc(BirthChart birthChart) {
-        for (HeavenCelestial planet : HeavenCelestial.values()) {
-            int position = birthChart.getPlanetPositionInt(planet.getName());
-            FourElements whatElement = FourElements.whatElement(position);
-            int strength = elementBalance.getOrDefault(whatElement, 0);
-            strength += planet.getValueToAdd();
-            elementBalance.put(whatElement, strength);
+        for (FourElements fourElements : FourElements.values()) {
+            elementBalance.put(fourElements, fourElements.elementStrength(birthChart));
         }
 
-        for (Houses houses : Houses.values()) {
-            int position = birthChart.getHousePositionInt(houses.getPositionNumber());
-            FourElements whatElement = FourElements.whatElement(position);
-            int strength = elementBalance.getOrDefault(whatElement, 0);
-            strength += houses.getValueToAdd();
-            elementBalance.put(whatElement, strength);
-        }
     }
 }
