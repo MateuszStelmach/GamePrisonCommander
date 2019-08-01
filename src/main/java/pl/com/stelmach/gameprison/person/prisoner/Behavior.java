da2b1f56ab7f897d10328f80e76a1a9e95602e6d
mateusz <matistelmach@gmail.com>
2019-08-01 12:38:27 +0200

    refactor

---
+++ src/main/java/pl/com/stelmach/gameprison/person/prisoner/Behavior.java
@@ -0,0 +1,25 @@
+package pl.com.stelmach.gameprison.person.prisoner;
+
+public interface Behavior {
+    boolean sex(int sexRate);
+
+    boolean suicide(int suicideRate);
+
+    boolean selfMutilation(int mutilationRate);
+
+    boolean inmateAttack(int attackRate);
+
+    boolean inmateRape(int rapeRate);
+
+    boolean guardAttack(int guardAttackRate);
+
+    boolean courtComplain(int courtComplainRate);
+
+    boolean mediaComplain(int mediaComplainRate);
+
+    boolean sendTreat(int treatRate);
+
+    boolean vandalism(int vandalismRate);
+
+
+}
---
+++ src/main/java/pl/com/stelmach/gameprison/person/prisoner/TrashPrisoner.java
@@ -0,0 +1,53 @@
+package pl.com.stelmach.gameprison.person.prisoner;
+
+public class TrashPrisoner implements Behavior {
+    @Override
+    public boolean sex(int sexRate) {
+        return false;
+    }
+
+    @Override
+    public boolean suicide(int suicideRate) {
+        return false;
+    }
+
+    @Override
+    public boolean selfMutilation(int mutilationRate) {
+        return false;
+    }
+
+    @Override
+    public boolean inmateAttack(int attackRate) {
+        return false;
+    }
+
+    @Override
+    public boolean inmateRape(int rapeRate) {
+        return false;
+    }
+
+    @Override
+    public boolean guardAttack(int guardAttackRate) {
+        return false;
+    }
+
+    @Override
+    public boolean courtComplain(int courtComplainRate) {
+        return false;
+    }
+
+    @Override
+    public boolean mediaComplain(int mediaComplainRate) {
+        return false;
+    }
+
+    @Override
+    public boolean sendTreat(int treatRate) {
+        return false;
+    }
+
+    @Override
+    public boolean vandalism(int vandalismRate) {
+        return false;
+    }
+}
