f933ee78927d9eed17dd151b9825a47d1cdd3d95
mateusz <matistelmach@gmail.com>
2019-08-08 16:19:54 +0200

    PriosnerRelation class

--- src/main/java/pl/com/stelmach/gameprison/person/prisoner/prisonerelation/PrisonerRelation.java
+++ src/main/java/pl/com/stelmach/gameprison/person/prisoner/prisonerelation/PrisonerRelation.java
@@ -2,11 +2,23 @@ package pl.com.stelmach.gameprison.person.prisoner.prisonerelation;
 
 import lombok.Getter;
 import pl.com.stelmach.gameprison.person.prisoner.PrisonerNumber;
-import pl.com.stelmach.gameprison.person.prisoner.prisonerelation.Relation;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @Getter
 public class PrisonerRelation {
     private Map<PrisonerNumber, Map<Relation, Integer>> prisonerRelations;
+
+    PrisonerRelation(List<PrisonerNumber> prisonerNumberList) {
+        prisonerRelations = new LinkedHashMap<>();
+        Map<Relation, Integer> relation = new LinkedHashMap<>();
+        for (PrisonerNumber prisonerNumber : prisonerNumberList) {
+            for (Relation rel : Relation.values()) {
+                relation.put(rel, 0);
+            }
+            prisonerRelations.put(prisonerNumber, relation);
+        }
+    }
 }
